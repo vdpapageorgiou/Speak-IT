4fc250db61b9574dafd40902965cc81fbc495efd
VickyFi <vickakifi@gmail.com>
2022-02-15 16:16:44 +0200

    first attempt of Speak IT App

---
+++ README.md
@@ -0,0 +1,26 @@
+# Speak IT App
+
+This app is based on the [Sample React App fot integration with the Azure Speech service](https://github.com/Azure-Samples/AzureSpeechReactSample) by [Azure-Samples](https://github.com/Azure-Samples).
+
+## Prerequisites
+
+1. This project assumes that you have an Azure account and Speech service subscription. If you don't have an account and subscription, [try the Speech service for free](https://docs.microsoft.com/azure/cognitive-services/speech-service/overview#try-the-speech-service-for-free).
+1. Ensure you have [Node.js](https://nodejs.org/en/download/) installed.
+
+## How to run the app
+
+1. Clone this repo, then change directory to the project root and run `npm install` to install dependencies.
+1. Run `cp .env.example .env`
+1. Add your Azure Speech key and region to the `.env` file, replacing the placeholder text.
+1. To run the Express server and React app together, run `npm run dev`.
+
+## Change recognition language
+
+To change the source recognition language, change the locale strings in `App.js` lines **32** and **66**, which sets the recognition language property on the `SpeechConfig` object.
+
+```javascript
+speechConfig.speechRecognitionLanguage = 'en-US'
+```
+
+For a full list of supported locales, see the [language support article](https://docs.microsoft.com/azure/cognitive-services/speech-service/language-support#speech-to-text).
+
---
+++ src/App.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { getTokenOrRefresh } from './token_util';
+import './custom.css'
+import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayText: 'INITIALIZED: ready to test speech...'
+        }
+    }
+    
+    async componentDidMount() {
+        // check for valid speech key/region
+        const tokenRes = await getTokenOrRefresh();
+        if (tokenRes.authToken === null) {
+            this.setState({
+                displayText: 'FATAL_ERROR: ' + tokenRes.error
+            });
+        }
+    }
+
+    async sttFromMic() {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+        
+        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        this.setState({
+            displayText: 'speak into your microphone...'
+        });
+
+        recognizer.recognizeOnceAsync(result => {
+            let displayText;
+            const synthesizer = new speechsdk.SpeechSynthesizer(speechConfig);
+       
+
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                //let words = "App, Color, Taxi, Navigation, Airport, Maps";
+                if(result.text.includes('taxi') || result.text.includes('Taxi') ){
+                  displayText = `Suggested app is: Text=${result.text}`;
+                }else if(result.text.includes('color') || result.text.includes('Color')){
+                  displayText = `Suggested app is Seeing AI by Microsoft`;
+                }
+                else{
+                  displayText = `RECOGNIZED: Text=${result.text}`;
+                  
+                }
+            } else {
+                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+            synthesizer.speakTextAsync(
+                displayText,
+                result => {
+                synthesizer.close();
+                return result.audioData;
+            },
+                error => {
+                console.log(error);
+                synthesizer.close();
+            });
+
+            this.setState({
+                displayText: displayText
+            });
+        });
+    }
+
+    async fileChange(event) {
+        const audioFile = event.target.files[0];
+        console.log(audioFile);
+        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+
+        this.setState({
+            displayText: fileInfo
+        });
+
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+
+        const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        recognizer.recognizeOnceAsync(result => {
+            let displayText;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                displayText = `RECOGNIZED: Text=${result.text}`
+    
+
+            } else {
+                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+
+            this.setState({
+                displayText: fileInfo + displayText
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Container className="app-container">
+                <div className="row align-items-md-stretch">
+                    <div className="col-md-6 sp-box">
+                        <div className="h-100 p-5 bg-light border shadow mb-5 bg-white rounded text-center d-flex align-items-center flex-column justify-content-center">
+                            <h2>Convert speech to text from your mic</h2>
+                            <div className="mic-btn position-relative">
+                                <i className="fas fa-microphone fa-lg text-center position-absolute" onClick={() => this.sttFromMic()}></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 sp-box">
+                        <div className="h-100 p-5 bg-light border shadow mb-5 bg-white rounded text-center d-flex align-items-center flex-column justify-content-center">
+                            <h2>Convert speech to text from an audio file</h2>
+                                <label htmlFor="audio-file" className="file-btn position-relative"><i className="fas fa-file-audio fa-lg text-center position-absolute"></i></label>
+                                    <input 
+                                        type="file" 
+                                        id="audio-file" 
+                                        onChange={(e) => this.fileChange(e)} 
+                                        style={{display: "none"}} 
+                                    />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mt-3">
+                    <div className="p-5 bg-light border shadow mb-5 bg-white rounded">
+                        <div>{this.state.displayText}</div>
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+}
